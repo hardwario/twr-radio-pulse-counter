83845f1ac9c608fe2bc28df9f2b1f3cfdfb5ed1e
konnev <petr.konvicka@bigclown.com>
2018-01-23 14:43:50 +0100

    Modified for radio

--- app/application.c
+++ app/application.c
@@ -1,153 +1,173 @@
 #include <application.h>
 
-#define BATTERY_VOLTAGE_DATA_STREAM_SAMPLES  6
-#define SIGFOX_FIRST_REPORT_SECONDS (10)
-#define SIGFOX_REPORT_INTERVAL_SECONDS (15 * 60)
-#define BATTERY_MODULE_UPDATE_INTERVAL_SECONDS (SIGFOX_REPORT_INTERVAL_SECONDS / BATTERY_VOLTAGE_DATA_STREAM_SAMPLES)
+#define REPORT_INTERVAL (5 * 60 * 1000)
+#define REPORT_INTERVAL_INITIAL (1 * 60 * 1000)
 
-#define HEADER_EVENT_ERROR 0xff
-#define HEADER_EVENT_UPDATE 0x00
+#define BATTERY_UPDATE_INTERVAL (60 * 60 * 1000)
 
-BC_DATA_STREAM_INT_BUFFER(stream_buffer_battery_voltage_mv, BATTERY_VOLTAGE_DATA_STREAM_SAMPLES)
-bc_data_stream_t stream_battery_voltage_mv;
+#define TEMPERATURE_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
+#define TEMPERATURE_PUB_VALUE_CHANGE 0.2f
+#define TEMPERATURE_UPDATE_INTERVAL (1 * 1000)
 
-bc_led_t led;
-bc_module_sigfox_t sigfox_module;
-bc_button_t button;
-uint8_t header = HEADER_EVENT_UPDATE;
-unsigned int channel_a_overflow_count = 0;
-unsigned int channel_b_overflow_count = 0;
-
-void pulse_counter_event_handler(bc_module_sensor_channel_t channel, bc_pulse_counter_event_t event, void *event_param)
+typedef struct
 {
-    (void) event_param;
+    uint8_t channel;
+    float value;
+    bc_tick_t next_pub;
 
-    if (event == BC_PULSE_COUNTER_EVENT_OVERFLOW)
-    {
-        if (channel == BC_MODULE_SENSOR_CHANNEL_A)
-        {
-            channel_a_overflow_count++;
-        }
-        else
-        {
-            channel_b_overflow_count++;
-        }
-    }
-}
+} event_param_t;
 
-void battery_module_event_handler(bc_module_battery_event_t event, void *event_param)
+static event_param_t temperature_event_param = { .next_pub = 0 };
+
+static struct
 {
-    (void) event_param;
+    bc_led_t led;
+    bc_button_t button;
+    float temperature;
+    bc_tag_temperature_t temperature_tag;
+    bc_button_t button_channel_b;
+    uint32_t button_channel_b_count;
 
-    if (event == BC_MODULE_BATTERY_EVENT_UPDATE)
-    {
-        float battery_voltage;
-        int battery_voltage_mv;
+} app;
 
-        bc_module_battery_update_voltage_on_battery(&battery_voltage);
-        battery_voltage_mv = battery_voltage * 1000;
+void pulse_counter_event_handler(bc_module_sensor_channel_t channel, bc_pulse_counter_event_t event, void *event_param);
 
-        bc_data_stream_feed(&stream_battery_voltage_mv, &battery_voltage_mv);
-    }
-}
+void battery_event_handler(bc_module_battery_event_t event, void *event_param);
 
-void sigfox_module_event_handler(bc_module_sigfox_t *self, bc_module_sigfox_event_t event, void *event_param)
-{
-	(void) self;
-	(void) event_param;
+void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
 
-	if (event == BC_MODULE_SIGFOX_EVENT_SEND_RF_FRAME_START)
-	{
-	    bc_led_pulse(&led, 100);
-	}
-	else if (event == BC_MODULE_SIGFOX_EVENT_ERROR)
-	{
-		bc_led_set_mode(&led, BC_LED_MODE_BLINK);
-	}
-}
+void temperature_tag_event_handler(bc_tag_temperature_t *self, bc_tag_temperature_event_t event, void *event_param);
 
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
-{
-	(void) self;
-	(void) event_param;
-
-	if (event == BC_BUTTON_EVENT_HOLD)
-	{
-		bc_scheduler_plan_now(0);
-	}
-}
+void button_channel_b_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
 
 void application_init(void)
 {
-    bc_data_stream_init(&stream_battery_voltage_mv, 1, &stream_buffer_battery_voltage_mv);
-
-	bc_led_init(&led, BC_GPIO_LED, false, false);
-	bc_led_set_mode(&led, BC_LED_MODE_FLASH);
+	bc_led_init(&app.led, BC_GPIO_LED, false, false);
+	bc_led_set_mode(&app.led, BC_LED_MODE_OFF);
 
 	bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_A, BC_PULSE_COUNTER_EDGE_FALL);
 	bc_pulse_counter_set_event_handler(BC_MODULE_SENSOR_CHANNEL_A, pulse_counter_event_handler, NULL);
-	bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_B, BC_PULSE_COUNTER_EDGE_FALL);
-    bc_pulse_counter_set_event_handler(BC_MODULE_SENSOR_CHANNEL_B, pulse_counter_event_handler, NULL);
 
-	bc_module_battery_init(BC_MODULE_BATTERY_FORMAT_MINI);
-	bc_module_battery_set_update_interval(BATTERY_MODULE_UPDATE_INTERVAL_SECONDS * 1000);
-    bc_module_battery_set_event_handler(battery_module_event_handler, NULL);
+    bc_module_sensor_set_mode(BC_MODULE_SENSOR_CHANNEL_B, BC_MODULE_SENSOR_MODE_INPUT);
+    bc_module_sensor_set_pull(BC_MODULE_SENSOR_CHANNEL_B, BC_MODULE_SENSOR_PULL_NONE);
+	bc_button_init(&app.button_channel_b, BC_GPIO_P5, BC_GPIO_PULL_UP, 1);
+	bc_button_set_event_handler(&app.button_channel_b, button_channel_b_event_handler, NULL);
 
-	bc_module_sigfox_init(&sigfox_module, BC_MODULE_SIGFOX_REVISION_R2);
-	bc_module_sigfox_set_event_handler(&sigfox_module, sigfox_module_event_handler, NULL);
+    // Initialize temperature
+    temperature_event_param.channel = BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_ALTERNATE;
+    bc_tag_temperature_init(&app.temperature_tag, BC_I2C_I2C0, BC_TAG_TEMPERATURE_I2C_ADDRESS_ALTERNATE);
+    bc_tag_temperature_set_update_interval(&app.temperature_tag, TEMPERATURE_UPDATE_INTERVAL);
+    bc_tag_temperature_set_event_handler(&app.temperature_tag, temperature_tag_event_handler, &temperature_event_param);
 
-    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
-    bc_button_set_event_handler(&button, button_event_handler, NULL);
+    // Initialize battery
+    bc_module_battery_init(BC_MODULE_BATTERY_FORMAT_MINI);
+    bc_module_battery_set_event_handler(battery_event_handler, NULL);
+    bc_module_battery_set_update_interval(BATTERY_UPDATE_INTERVAL);
 
-	bc_scheduler_plan_absolute(0, SIGFOX_FIRST_REPORT_SECONDS * 1000);
+    bc_button_init(&app.button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
+    bc_button_set_event_handler(&app.button, button_event_handler, NULL);
+
+    bc_radio_init(BC_RADIO_MODE_NODE_SLEEPING);
+
+    bc_scheduler_plan_relative(0, 3000);
+
+    bc_radio_pairing_request("wireless-pulse-counter", "v1.1.0");
+
+    bc_led_pulse(&app.led, 2000);
 }
 
 void application_task(void *param)
 {
 	(void) param;
 
-	if (!bc_module_sigfox_is_ready(&sigfox_module))
-	{
-		bc_scheduler_plan_current_now();
-		return;
-	}
-
-    uint8_t buffer[12];
     uint32_t channel_count_a;
-    uint32_t channel_count_b;
-    int battery_voltage;
-	uint16_t battery_voltage_mv;
 
     channel_count_a = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_A);
-    channel_count_b = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_B);
-	bc_data_stream_get_average(&stream_battery_voltage_mv, &battery_voltage);
 
-	if ((channel_a_overflow_count > 0xff) || (channel_b_overflow_count > 0xff))
-	{
-	    header = HEADER_EVENT_ERROR;
-	}
+    bc_radio_pub_uint32("pulse-counter/a/count", &channel_count_a);
 
-	battery_voltage_mv = (uint16_t)battery_voltage;
+    uint32_t channel_event_count_b;
 
-    buffer[0] = header;
-    buffer[1] = (((channel_a_overflow_count << 4) & 0xf0) | ((channel_b_overflow_count) & 0x0f));
-    buffer[2] = channel_count_a;
-    buffer[3] = channel_count_a >> 8;
-    buffer[4] = channel_count_a >> 16;
-    buffer[5] = channel_count_a >> 24;
-    buffer[6] = channel_count_b;
-    buffer[7] = channel_count_b >> 8;
-    buffer[8] = channel_count_b >> 16;
-    buffer[9] = channel_count_b >> 24;
-    buffer[10] = battery_voltage_mv;
-    buffer[11] = battery_voltage_mv >> 8;
+    channel_event_count_b = app.button_channel_b_count;
 
-	if (bc_module_sigfox_send_rf_frame(&sigfox_module, &buffer, sizeof(buffer)))
-	{
-		bc_scheduler_plan_current_relative(SIGFOX_REPORT_INTERVAL_SECONDS * 1000);
-	}
-	else
-	{
-		bc_scheduler_plan_current_relative(1000);
-	}
+    bc_radio_pub_uint32("push-button/b/event_count", &channel_event_count_b);
+
+    bc_scheduler_plan_current_relative(REPORT_INTERVAL);
+}
+
+void pulse_counter_event_handler(bc_module_sensor_channel_t channel, bc_pulse_counter_event_t event, void *event_param)
+{
+    (void) channel;
+    (void) event_param;
+
+    if (event == BC_PULSE_COUNTER_EVENT_OVERFLOW)
+    {
+        // TODO
+    }
+}
+
+void battery_event_handler(bc_module_battery_event_t event, void *event_param)
+{
+    (void) event;
+    (void) event_param;
+
+    float voltage;
+
+    if (bc_module_battery_get_voltage(&voltage))
+    {
+        bc_radio_pub_battery(&voltage);
+    }
+}
+
+void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
+{
+    (void) self;
+    (void) event_param;
+
+    if (event == BC_BUTTON_EVENT_PRESS)
+    {
+        bc_led_pulse(&app.led, 100);
+
+        static uint16_t event_count = 0;
+
+        event_count++;
+
+        bc_radio_pub_push_button(&event_count);
+    }
+}
+
+void temperature_tag_event_handler(bc_tag_temperature_t *self, bc_tag_temperature_event_t event, void *event_param)
+{
+    float value;
+    event_param_t *param = (event_param_t *)event_param;
+
+    if (event == BC_TAG_TEMPERATURE_EVENT_UPDATE)
+    {
+        if (bc_tag_temperature_get_temperature_celsius(self, &value))
+        {
+            if ((fabs(value - param->value) >= TEMPERATURE_PUB_VALUE_CHANGE) || (param->next_pub < bc_scheduler_get_spin_tick()))
+            {
+                bc_radio_pub_temperature(param->channel, &value);
+
+                param->value = value;
+                param->next_pub = bc_scheduler_get_spin_tick() + TEMPERATURE_PUB_NO_CHANGE_INTEVAL;
+            }
+        }
+    }
+}
+
+void button_channel_b_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
+{
+    if (event == BC_BUTTON_EVENT_CLICK)
+    {
+        bc_led_pulse(&app.led, 100);
+
+        app.button_channel_b_count++;
+
+        bc_radio_pub_uint32("push-button/b/event_count", &app.button_channel_b_count);
+    }
+    else if (event == BC_BUTTON_EVENT_HOLD)
+    {
+        bc_scheduler_plan_now(0);
+    }
 }
